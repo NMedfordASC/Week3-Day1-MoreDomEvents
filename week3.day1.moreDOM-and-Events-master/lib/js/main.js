f91dbb55aea85455d3e9e079d5cb1979dca9a57e
NMedfordASC <52670483+NMedfordASC@users.noreply.github.com>
2019-08-02 10:08:37 -0400

    Add files via upload

---
+++ week3.day1.moreDOM-and-Events-master/lib/js/main.js
@@ -0,0 +1,8 @@
+dragElement(document.getElementById("weapons"));
+ function dragElement(elem){
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if(document.getElementById("weapons")){
+      document.getElementById(elem)  
+    }
+    
+}
